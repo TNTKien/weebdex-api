ced38dec7e8d5517c0a8249da7155389fd5ff5e2
iamneyk <iamneyk@gmail.com>
2026-02-24 16:08:52 +0700

    refactor: update constants and add cover image endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
 
-const API_BASE = 'https://api.weebdex.org'
+const TARGET_BASE = 'https://api.weebdex.org'
+const WEEBDEX_ORIGIN = 'https://weebdex.org'
+const WEEBDEX_REFERER = 'https://weebdex.org/'
+const WEEBDEX_COVER_ORIGIN = 'https://srv.weebdex.net'
 
 const PASSTHROUGH_HEADERS = [
   'authorization',
@@ -24,13 +27,38 @@ app.use(
   })
 )
 
-app.all('*', async (c) => {
-  const url = new URL(c.req.url)
-  const targetUrl = `${API_BASE}${url.pathname}${url.search}`
+// Cover images
+app.get('/covers/:mangaid/:filename', async (c) => {
+  const { mangaid, filename } = c.req.param()
+  const targetUrl = `${WEEBDEX_COVER_ORIGIN}/covers/${mangaid}/${filename}`
 
   const headers = new Headers({
-    origin: 'https://weebdex.org',
-    referer: 'https://weebdex.org/',
+    origin: WEEBDEX_ORIGIN,
+    referer: WEEBDEX_REFERER,
+  })
+
+  const response = await fetch(targetUrl, {
+    method: 'GET',
+    headers,
+  })
+
+  const responseHeaders = new Headers(response.headers)
+  responseHeaders.delete('content-encoding')
+
+  return new Response(response.body, {
+    status: response.status,
+    headers: responseHeaders,
+  })
+})
+
+// Other API endpoints
+app.all('*', async (c) => {
+  const url = new URL(c.req.url)
+  const targetUrl = `${TARGET_BASE}${url.pathname}${url.search}`
+
+  const headers = new Headers({
+    origin: WEEBDEX_ORIGIN,
+    referer: WEEBDEX_REFERER,
   })
 
   for (const name of PASSTHROUGH_HEADERS) {
